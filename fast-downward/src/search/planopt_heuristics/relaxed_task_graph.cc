619fa073324967be6f43e37ecfbf0673ac78fe54
Izaque Dione Nunes <68032837+IzaqueNunes@users.noreply.github.com>
2024-07-18 16:05:25 -0300

    Update relaxed_task_graph.cc

--- fast-downward/src/search/planopt_heuristics/relaxed_task_graph.cc
+++ fast-downward/src/search/planopt_heuristics/relaxed_task_graph.cc
@@ -9,36 +9,38 @@ namespace planopt_heuristics {
 RelaxedTaskGraph::RelaxedTaskGraph(const TaskProxy &task_proxy)
     : relaxed_task(task_proxy),
       variable_node_ids(relaxed_task.propositions.size()) {
- 		
- 		// Adicionando nó incial e o nó objetivo no grafo, como tipo OR
- 		initial_node_id = graph.add_node(NodeType::OR);
-    	goal_node_id = graph.add_node(NodeType::OR);
-    	
-	    	// Para cada proposição é criado um nó no grafo e seu ID é armazenado
-	    for (size_t i = 0; i < relaxed_task.propositions.size(); ++i) {
-	        variable_node_ids[i] = graph.add_node(NodeType::OR);
-	    }
-	    
-		    // Adicionando arestas de todos os operadores da tarefa relaxada
-	    for (const auto &op : relaxed_task.operators) {
-	        // Criando nó para o operador
-	        int operator_node_id = graph.add_node(NodeType::AND);
-	
-	        // Adicionando arestas das proposições de pré-condição para o nó do operador
-	        for (PropositionID precond_id : op.preconditions) {
-	            graph.add_edge(variable_node_ids[precond_id], operator_node_id);
-	        }
-	
-	        // Adicionando arestas do nó do operador para as proposições de efeito
-	        for (PropositionID effect_id : op.effects) {
-	            graph.add_edge(operator_node_id, variable_node_ids[effect_id]);
-	        }
-	    }
- 
+    
+    // Adicionando nó inicial e o nó objetivo no grafo, como tipo AND
+    initial_node_id = graph.add_node(NodeType::AND);
+    goal_node_id = graph.add_node(NodeType::AND);
+    
+    // Para cada proposição é criado um nó no grafo e seu ID é armazenado
+    for (size_t i = 0; i < relaxed_task.propositions.size(); ++i) {
+        variable_node_ids[i] = graph.add_node(NodeType::OR);
+    }
+    
+    // Adicionando arestas de todos os operadores da tarefa relaxada
+    for (const auto &op : relaxed_task.operators) {
+        // Criando nó para o operador
+        int operator_node_id = graph.add_node(NodeType::AND);
+        
+        // Adicionando arestas das proposições de pré-condição para o nó do operador
+        for (PropositionID precond_id : op.preconditions) {
+            graph.add_edge(variable_node_ids[precond_id], operator_node_id);
+        }
+        
+        // Adicionando arestas do nó do operador para as proposições de efeito
+        for (PropositionID effect_id : op.effects) {
+            graph.add_edge(operator_node_id, variable_node_ids[effect_id]);
+            
+            // Ajustando o custo direto do nó de efeito
+            graph.set_direct_cost(variable_node_ids[effect_id], op.cost);
+        }
+    }
 }
 
 void RelaxedTaskGraph::change_initial_state(const GlobalState &global_state) {
-    // Remove all initial edges that where introduced for relaxed_task.initial_state.
+    // Remove all initial edges that were introduced for relaxed_task.initial_state.
     for (PropositionID id : relaxed_task.initial_state) {
         graph.remove_edge(variable_node_ids[id], initial_node_id);
     }
@@ -61,7 +63,7 @@ bool RelaxedTaskGraph::is_goal_relaxed_reachable() {
     // Obtendo o nó de meta no gráfico
     const AndOrGraphNode &goal_node = graph.get_node(goal_node_id); 
     
-    return graph.get_node(goal_node_id).forced_true;
+    return goal_node.forced_true;
 }
 
 int RelaxedTaskGraph::additive_cost_of_goal() {
